ea525195cb4b1b300fe206ddbcc431b06eb1f4ce
shaexintran <shaexin@icloud.com>
2025-05-27 13:16:03 -0700

    exercise 4 now takes in all 3 request queries

--- server.js
+++ server.js
@@ -38,6 +38,9 @@ app.get('/collectibles/:idx', (req, res) => {
 })
 
 /* ---------------------- Exercise 4 ---------------------- */
+    // a function that takes 3 values, shoetype, minprice, and maxprice
+    // sends back an array based on whether those were true or not
+    // send the array with a check
 
 const shoes = [
     { name: "Birkenstocks", price: 50, type: "sandal" },
@@ -50,19 +53,48 @@ const shoes = [
 ]
 
 app.get('/shoes', (req, res) => {
-    if (req.query.type) {
-        const shoeTypes = shoes.filter(shoe => shoe.type === req.query.type)
-        shoeTypes.length > 0 ? res.send(shoeTypes) : res.send('We do not have this type of shoe.')
-    }
-    else if (req.query.minprice) {
-        const shoesAboveMinPrice = shoes.filter(shoe => shoe.price >= req.query.minprice)
-        shoesAboveMinPrice.length > 0 ? res.send(shoesAboveMinPrice) : res.send(`No shoes with a mininum price of ${req.query.minprice}.`)
-    }
-    else if (req.query.maxprice) {
-        const shoesUnderMaxPrice = shoes.filter(shoe => shoe.price <= req.query.maxprice)
-        shoesUnderMaxPrice.length > 0 ? res.send(shoesUnderMaxPrice) : res.send(`No shoes under a maximum price of ${req.query.maxprice}.`)
-    }
-    else {
+    const shoeType = req.query.type ? req.query.type : null
+    const minPrice = req.query["min-price"] ? Number(req.query["min-price"]) : null
+    const maxPrice = req.query["max-price"] ? Number(req.query["max-price"]) : null
+
+    if (!shoeType && !minPrice && !maxPrice) {
         res.send(shoes)
+        return
+    }
+
+    let shoesByType, aboveMinPrice, belowMaxPrice, finalList
+    let queryNullCount = 0
+    const queryArray = [shoeType, minPrice, maxPrice]
+    queryArray.forEach(q => {if (!q) queryNullCount += 1})
+
+    if (queryNullCount < 2) {
+        if (shoeType && minPrice && maxPrice) {
+            shoesByType = shoes.filter(shoe => shoe.type === shoeType)
+            aboveMinPrice = shoesByType.filter(shoe => shoe.price >= minPrice)
+            finalList = aboveMinPrice.filter(shoe => shoe.price <= maxPrice)
+        } else if (shoeType && minPrice && !maxPrice) {
+            shoesByType = shoes.filter(shoe => shoe.type === shoeType)
+            finalList = shoesByType.filter(shoe => shoe.price >= minPrice)
+        } else if (shoeType && !minPrice && maxPrice) {
+            shoesByType = shoes.filter(shoe => shoe.type === shoeType)
+            finalList = shoesByType.filter(shoe => shoe.price <= maxPrice)
+        } else if (!shoeType && minPrice && maxPrice) {
+            aboveMinPrice = shoes.filter(shoe => shoe.price >= minPrice)
+            finalList = aboveMinPrice.filter(shoe => shoe.price <= maxPrice)
+        }
+        finalList.length > 0 ? res.send(finalList) : res.send('Sorry, no matches found.')
+    } else {
+        const queryFilter = queryArray.find(q => q !== null)
+        switch (queryFilter) {
+            case shoeType:
+                res.send(shoes.filter(shoe => shoe.type === shoeType))
+                break
+            case minPrice:
+                res.send(shoes.filter(shoe => shoe.price >= minPrice))
+                break
+            case maxPrice:
+                res.send(shoes.filter(shoe => shoe.price <= maxPrice))
+                break
+        }
     }
 })
